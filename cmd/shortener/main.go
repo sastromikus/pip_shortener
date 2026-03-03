51bfd5ed56c6919978055906d31bab70d1197b67
pipobukapi <savotiya@gmail.com>
2026-03-03 07:44:18 +0300

    chore: add shortener entrypoint

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type badRequestHandler struct{}
+
+func (badRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+}
+
+func main() {
+	srv := &http.Server{
+		Addr:              ":8080",
+		Handler:           badRequestHandler{},
+	}
+
+	go func() {
+		log.Println("listening")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_ = srv.Shutdown(ctx)
+	log.Println("shutdown")
+}
