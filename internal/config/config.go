425238ba1e42118b3b6f19778d0bc28273464884
pipobukapi <savotiya@gmail.com>
2026-04-23 08:51:25 +0300

    add file storage config flag and env

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,24 +8,32 @@ import (
 type Config struct {
 	ServerAddr string
 	BaseURL    string
+	FileStoragePath string
 }
 
 const (
-	defaultServerAddr = "localhost:8080"
-	defaultBaseURL    = "http://localhost:8080"
+	defaultServerAddr 		= "localhost:8080"
+	defaultBaseURL    		= "http://localhost:8080"
 
-	envServerAddr = "SERVER_ADDRESS"
-	envBaseURL    = "BASE_URL"
+	envServerAddr 			= "SERVER_ADDRESS"
+	envBaseURL    			= "BASE_URL"
+
+	defaultFileStoragePath 	= "storage.json"
+	envFileStoragePath 		= "FILE_STORAGE_PATH"
 )
 
 func Parse() Config {
 	cfg := Config{
-		ServerAddr: defaultServerAddr,
-		BaseURL:    defaultBaseURL,
+		ServerAddr:      defaultServerAddr,
+		BaseURL:		 defaultBaseURL,
+		FileStoragePath: defaultFileStoragePath,
 	}
 
 	var flagAddr string
 	var flagBase string
+	var flagFile string
+
+	flag.StringVar(&flagFile, "f", "", "File storage path")
 	flag.StringVar(&flagAddr, "a", "", "HTTP server address")
 	flag.StringVar(&flagBase, "b", "", "Base URL for short links")
 	flag.Parse()
@@ -36,12 +44,18 @@ func Parse() Config {
 	if flagBase != "" {
 		cfg.BaseURL = flagBase
 	}
+	if flagFile != "" {
+		cfg.FileStoragePath = flagFile
+	}
 
 	if v := os.Getenv(envServerAddr); v != "" {
 		cfg.ServerAddr = v
 	}
 	if v := os.Getenv(envBaseURL); v != "" {
 		cfg.BaseURL = v
+	}
+	if v := os.Getenv(envFileStoragePath); v != "" {
+		cfg.FileStoragePath = v
 	}
 
 	return cfg
