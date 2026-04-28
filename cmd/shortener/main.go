57ac70d9945616e3d525c6addb5924f6f3a4da6c
pipobukapi <savotiya@gmail.com>
2026-04-29 00:21:07 +0300

    start delete worker

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -60,6 +60,7 @@ func run() error {
 	}
 
 	svc := service.NewShortener(repo)
+	svc.StartDeleteWorker(128, 500 * time.Millisecond)
 	router := handler.NewRouter(svc, cfg.BaseURL, logger, db)
 
 	srv := &http.Server{
