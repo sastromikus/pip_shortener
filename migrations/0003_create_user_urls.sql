210620195c1dfd041288ca2285349cfb26ff28e9
pipobukapi <savotiya@gmail.com>
2026-05-27 12:17:52 +0300

    fix migration

--- migrations/0003_create_user_urls.sql
+++ migrations/0003_create_user_urls.sql
@@ -1,9 +1,5 @@
 CREATE TABLE IF NOT EXISTS user_urls (
     user_id TEXT NOT NULL,
     short_id VARCHAR(32) NOT NULL,
-    PRIMARY KEY (user_id, short_id),
-    CONSTRAINT user_urls_short_id_fk
-        FOREIGN KEY (short_id)
-        REFERENCES urls (short_id)
-        ON DELETE CASCADE
+    PRIMARY KEY (user_id, short_id)
 );
